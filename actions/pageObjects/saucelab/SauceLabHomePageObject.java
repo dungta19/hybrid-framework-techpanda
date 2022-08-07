8e60c41e477db0f16f4415f23fd994e163ad4d0e
Dung Ta <ttdung1901@gmail.com>
2022-08-07 15:09:19 +0700

    Framework - Sort Asc/Des by String/Float/Date

---
+++ actions/pageObjects/saucelab/SauceLabHomePageObject.java
@@ -0,0 +1,38 @@
+package pageObjects.saucelab;
+
+import commons.BasePage;
+import org.openqa.selenium.WebDriver;
+import pageUIs.saucelab.SauceLabHomePageObjectUI;
+
+public class SauceLabHomePageObject extends BasePage {
+    WebDriver driver;
+
+    public SauceLabHomePageObject(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void sortItemByText(String valueToSellect) {
+        waitForElementVisible(driver, SauceLabHomePageObjectUI.SAUCELAB_SORT_DROPDOWN);
+        selectItemInDefaultDropDown(driver, SauceLabHomePageObjectUI.SAUCELAB_SORT_DROPDOWN, valueToSellect);
+    }
+
+    public boolean isProductNamesSortedAscending() {
+        waitForElementVisible(driver, SauceLabHomePageObjectUI.SAUCELAB_PRODUCT_NAMES);
+        return isDataTextSortedCorrectly(driver, SauceLabHomePageObjectUI.SAUCELAB_PRODUCT_NAMES, true, false);
+    }
+
+    public boolean isProductNamesSortedDescending() {
+        waitForElementVisible(driver, SauceLabHomePageObjectUI.SAUCELAB_PRODUCT_NAMES);
+        return isDataTextSortedCorrectly(driver, SauceLabHomePageObjectUI.SAUCELAB_PRODUCT_NAMES, false, true);
+    }
+
+    public boolean isProductPricesSortedAscending() {
+        waitForElementVisible(driver, SauceLabHomePageObjectUI.SAUCELAB_PRODUCT_PRICES);
+        return isDataFloatSortedCorrectly(driver, SauceLabHomePageObjectUI.SAUCELAB_PRODUCT_PRICES, true, false);
+    }
+
+    public boolean isProductPriceSortedDescending() {
+        waitForElementVisible(driver, SauceLabHomePageObjectUI.SAUCELAB_PRODUCT_PRICES);
+        return isDataFloatSortedCorrectly(driver, SauceLabHomePageObjectUI.SAUCELAB_PRODUCT_PRICES, false, true);
+    }
+}
